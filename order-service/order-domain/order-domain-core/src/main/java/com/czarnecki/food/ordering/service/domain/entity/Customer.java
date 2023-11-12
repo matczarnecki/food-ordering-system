60ee4a4b50706032e914de53daa215488ae6f899
Mateusz Czarnecki <mateusz.czarnecki@softiq.com>
2023-11-12 20:14:57 +0100

    feat: added customer and restaurant entities

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/czarnecki/food/ordering/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.czarnecki.food.ordering.service.domain.entity;
+
+import com.czarnecki.food.ordering.domain.entity.AggregateRoot;
+import com.czarnecki.food.ordering.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
