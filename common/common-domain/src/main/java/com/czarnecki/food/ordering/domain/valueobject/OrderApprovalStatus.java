e92488118aaac393d366e81921fac721a2146dbc
Mateusz Czarnecki <mateusz.czarnecki@softiq.com>
2023-11-13 19:39:24 +0100

    feat: order application service dto classes

---
+++ common/common-domain/src/main/java/com/czarnecki/food/ordering/domain/valueobject/OrderApprovalStatus.java
@@ -0,0 +1,5 @@
+package com.czarnecki.food.ordering.domain.valueobject;
+
+public enum OrderApprovalStatus {
+    APPROVED, REJECTED
+}
---
+++ common/common-domain/src/main/java/com/czarnecki/food/ordering/domain/valueobject/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.czarnecki.food.ordering.domain.valueobject;
+
+public enum PaymentStatus {
+    COMPLETED, CANCELLED, FAILED
+}
